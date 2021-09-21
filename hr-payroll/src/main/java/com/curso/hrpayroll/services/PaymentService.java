cdfa90ee86b0a3c08dd24227ac5b5b4e62afeea0
diego.dantas <diego.dantas@radixeng.com.br>
2021-09-21 18:47:07 -0300

    hr-payroll Implementando o spring cloud Ribbon

--- hr-payroll/src/main/java/com/curso/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/curso/hrpayroll/services/PaymentService.java
@@ -1,12 +1,7 @@
 package com.curso.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.curso.hrpayroll.entities.Payment;
 import com.curso.hrpayroll.entities.Worker;
@@ -23,9 +18,4 @@ public class PaymentService {
 		Worker worker = workerFeignClient.findbyId(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome() ,days);
 	}
-	
-//	public Payment getPayment(long workerId, int days) {
-//		
-//	}
-
 }
