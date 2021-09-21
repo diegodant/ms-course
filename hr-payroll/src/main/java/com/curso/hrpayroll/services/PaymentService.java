63f7bb5ecbe0089b628171d9158197bf2a5b27a8
diego.dantas <diego.dantas@radixeng.com.br>
2021-09-21 11:57:39 -0300

    hr-payroll implementando o serviço e o mock

---
+++ hr-payroll/src/main/java/com/curso/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.curso.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.curso.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0,days);
+	}
+
+}
