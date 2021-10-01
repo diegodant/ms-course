c4312c6f75c8c4aa70bb6c4d1172c7672afbb0e7
diego.dantas <diego.dantas@radixeng.com.br>
2021-10-01 15:02:14 -0300

    implementando a toleracia de falhas no hystrix

--- hr-worker/src/main/java/com/curso/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/curso/hrworker/resources/WorkerResource.java
@@ -36,6 +36,15 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findbyId(@PathVariable Long id){
+		
+//		teste para ver se hysterix estava interceptando o metodo
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		Worker obj = repository.findById(id).get();
 		
