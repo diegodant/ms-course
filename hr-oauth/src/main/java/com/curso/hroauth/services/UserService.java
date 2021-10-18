ea722987d97b3d65bc49da472af879368a3ea7ae
diego.dantas <diego.dantas@radixeng.com.br>
2021-10-18 17:51:34 -0300

    Feita a configuraçao do projeto hr-oauth acessando o hruser com o feignClient

---
+++ hr-oauth/src/main/java/com/curso/hroauth/services/UserService.java
@@ -0,0 +1,29 @@
+package com.curso.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.hroauth.entities.User;
+import com.curso.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findbyEmail(email).getBody();
+		
+		if(user == null) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.error("Email found: " + email);
+		return user;
+	}
+}
