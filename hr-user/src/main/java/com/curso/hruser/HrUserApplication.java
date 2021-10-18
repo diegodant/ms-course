8bc338280063d58736956bddac0e66f92fa65c8b
diego.dantas <diego.dantas@radixeng.com.br>
2021-10-18 11:06:31 -0300

    Criado classe de configuracao para componente injetavel que nao esta instanciado no spring e arquivo .sql das entidades

--- hr-user/src/main/java/com/curso/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/curso/hruser/HrUserApplication.java
@@ -1,13 +1,27 @@
 package com.curso.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner{
 
+	//OBS essa classe nao é instanciada por padrao no SPRING, por isso
+	// é necessario instanciar em um classe de configuracao(AppConfig) do spring e e injetar o Bean
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		//System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
 	}
 
 }
